ab07ba2e862fb652d969cfd1b135d966e4994534
shufu <fsq19981210@gmail.com>
2021-02-26 21:26:22 -0500

    Initial commit

---
+++ src/main/java/com/quan/BadgeApplication.java
@@ -0,0 +1,19 @@
+package com.quan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@SpringBootApplication
+public class BadgeApplication {
+
+    @Bean
+    BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(BadgeApplication.class, args);
+    }
+}
